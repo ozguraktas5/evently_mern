60755bcf8a6b17db6ee37331a4aeb78d859f8c6f
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2023-12-18 20:07:57 +0300

    18.12.2023

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='border-t'>
+      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
+        <Link href="/">
+          <Image 
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p>2023 Evently. All rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
